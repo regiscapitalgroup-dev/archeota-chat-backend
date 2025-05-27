da7d3dc9f3dd9e329c968d207bcb56a75a92977b
Omar Espinoza <omar.espinoza@outlook.com>
2025-05-27 13:30:41 -0700

    database updated

--- archeota/settings.py
+++ archeota/settings.py
@@ -81,18 +81,18 @@ WSGI_APPLICATION = 'archeota.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 #DATABASES = {
-#  'default': dj_database_url.config(
-#       default=os.getenv('DATABASE_URL')
-#    )
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
 #}
+
+DATABASES = {
+  'default': dj_database_url.config(
+       default=os.getenv('DATABASE_URL')
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
